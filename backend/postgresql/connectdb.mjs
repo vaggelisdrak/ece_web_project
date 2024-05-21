f7d24f758b9fb4c91df4b267435e2284604c5a19
vaggelisdrak <v.drakontaeidis@gmail.com>
2024-05-21 17:47:28 +0300

    fixed db connection

--- backend/postgresql/connectdb.mjs
+++ backend/postgresql/connectdb.mjs
@@ -6,12 +6,31 @@ const { Pool } = pkg;
 
 dotenv.config(); 
 
-const pool = new Pool({
+/*const pool = new Pool({
     user: "postgres",
     host: "localhost",
     database: "postgres",
     password: process.env.POSTGRESQL_PASSWORD,
     port: 5432,
+});*/
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false // This may be necessary for some hosted PostgreSQL services
+    }
 });
+
+// Example test function to verify connection
+const testConnection = async () => {
+    try {
+        const client = await pool.connect();
+        console.log('Connected to the database successfully!');
+        client.release();
+    } catch (err) {
+        console.error('Error connecting to the database:', err);
+    }
+};
+
 
 export default pool;
